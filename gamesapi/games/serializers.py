c3b28dd8b5515be6f5a1fc2f238d6dcc990e3830
ofelipegabriel321 <ofelipegabriel321@gmail.com>
2019-10-21 14:01:24 -0300

    Adição de validaters em serializers para o atributo name e o método delete

--- gamesapi/games/serializers.py
+++ gamesapi/games/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from games.models import Game
+from datetime import datetime
 
 class GameSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,3 +11,16 @@ class GameSerializer(serializers.ModelSerializer):
                   'release_date',
                   'game_category',
                   'played')
+
+    @staticmethod
+    def validate_name(value):
+        games = Game.objects.filter(name=value)
+        if games:
+            raise serializers.ValidationError("Não podem haver dois jogos com o mesmo nome.")
+        return value
+    
+    @staticmethod
+    def validate_delete(game):
+        if game.release_data <= datetime.now():
+            raise serializers.ValidationError("Um jogo já lançado não pode ser excluído.")
+        return game
